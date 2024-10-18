699a2a8df77533dd4649b8a369927a734c84d70d
Hristina <h.videva9484@gmail.com>
2024-10-18 16:15:09 +0300

    Seccond commit

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
         int[][] table = new int[size][size];
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                String valueInput = JOptionPane.showInputDialog("Enter value for element [" + i + "][" + j + "]:");
+                String valueInput = JOptionPane.showInputDialog("Enter value for element [" +  (i + 1) + "][" + (j +1 ) + "]:");
                 table[i][j] = Integer.parseInt(valueInput);
             }
         }
@@ -62,24 +62,24 @@ public class Main {
 
         //#region Cycling the rest of the LPC tables
         for(int n = 0; n < size - 1; n++){
-            int min = 100;
-            int i = first - 1;
+            int min = C[first - 1];
+            int minI = first - 1;
             for(int j = 0; j < size; j++){
-                if(L[j] != 0 && min > C[j]){
+                if(L[j] == 0 && min > C[j]){
                     min = C[j];
-                    i = j;
+                    minI = j;
                 }
             }
             for(int j = 0; j < size; j++){
-                if(L[j] != 0) {
-                    int pC = C[i] + table[i][j];
+                if(L[j] == 0) {
+                    int pC = min + table[minI][j];
                     if (pC < C[j]){
-                        P[j] = i;
+                        P[j] = minI;
                         C[j] = pC;
                     }
                 }
             }
-            L[i] = 1;
+            L[minI] = 1;
             for(int k = 0; k < size; k++){
                 result.append(L[k]).append(" ");
             }
